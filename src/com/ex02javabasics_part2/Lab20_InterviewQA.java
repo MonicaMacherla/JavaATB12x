917598fbb2708cfcf11ad70f18bd8422ef7ccdbf
Hemalatha <monicamacherla3@gmail.com>
2025-05-21 08:06:41 +0530

    21-05-2025

---
+++ src/com/ex02javabasics_part2/Lab20_InterviewQA.java
@@ -0,0 +1,12 @@
+package com.ex02javabasics_part2;
+
+public class Lab20_InterviewQA {
+    public static void main (String[] args){
+
+         //int enum=10; //enum is a key word cant'be used as identifier
+        int Enum=10;
+        //int monica macherla=110; //space not allowed in variable name
+        int monica_macherla=110;
+        int Lab20_InterviewQA=123;
+    }
+}
---
+++ src/com/ex02javabasics_part2/Lab21_Boolean.java
@@ -0,0 +1,11 @@
+package com.ex02javabasics_part2;
+
+public class Lab21_Boolean {
+    public static void main (String[] args){
+        boolean a=true;
+        boolean b=false;
+        boolean is_long_variable_name_allowed=true;
+
+        System.out.println(a);
+    }
+}
