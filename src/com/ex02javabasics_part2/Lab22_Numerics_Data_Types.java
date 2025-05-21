1beb027e6cd5b4e906b3d06be2b1cd51a0bff711
Hemalatha <monicamacherla3@gmail.com>
2025-05-21 09:00:19 +0530

    21-05-2025

---
+++ src/com/ex02javabasics_part2/Lab22_Numerics_Data_Types.java
@@ -0,0 +1,16 @@
+package com.ex02javabasics_part2;
+
+public class Lab22_Numerics_Data_Types {
+    public static void main (String[] args){
+      //  byte a=128; //-128 to 127
+        byte a=125;
+        int i=1243;
+        short s=1235;
+        long l=9246262919l;
+        long l1=9246262929L;
+        char c='A';
+        float f=31.4f;
+        float f1=3.14F;
+
+    }
+}
